9e14bd6c3a021ce1aa101243c77b3e784ebdd9c2
unknown <alexander.norberghoff@gmail.com>
2019-12-09 21:15:58 +0100

    deploy prep

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { ApolloServer, gql } = require('apollo-server-express')
 const express = require('express')
 
@@ -7,7 +8,6 @@ const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
 
 const app = express()
-
 app.disable('x-powered-by')
 
 const server = new ApolloServer({
@@ -20,6 +20,15 @@ const server = new ApolloServer({
    })
 })
 
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'))
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(
+         __dirname, 'client', 'build', 'index.html'
+      ))
+   })
+}
+
 server.applyMiddleware({ app })
 
-app.listen({ port: 4000 })
+app.listen({ port: process.env.PORT || 4000 })
